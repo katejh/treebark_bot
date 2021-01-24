aff84504b29ef7032ec26e858fc5fd8359457545
katejh <katejhuang@hotmail.com>
2021-01-24 01:37:01 -0800

    roughly implemented some commands

---
+++ README.md
@@ -0,0 +1,34 @@
+# TreeBark helper bot
+
+## Setting up
+
+Pull this repo or something
+Set up a .env file and set DISCORD_TOKEN
+Set DATABASE_URL (no need if Heroku is set up)
+
+## Deploying to Heroku
+
+## Accessing database
+
+In command line, run `heroku pg:psql`
+Then in session do SQL commands and stuff
+
+## Usage
+
+Commands
+- [ ] `treebark` or `treebark help` - lists available commands
+- [x] `treebark coords <world_name> <coords_tag (optional)>` - gets saved coordinates of world. world_name must not have any spaces, coords_tag should not have spaces either.
+- [] `treebark record <world_name> <coords_tag> <x> <y> <z> <description (optional)>` - records new coordinates
+- [ ] `treebark update <world_name> <coords_tag> <description>` updates description of coords
+- [ ] `treebark delete <world_name> <coords_tag>` - deletes coordinates
+- [ ] `treebark worlds` - lists available worlds and saved coords associated with each
+
+## TODOs
+
+- move to AWS
+- refactor for cleaner code and more OOP-oriented approach. also be consistent with code
+- README with installation, setup, usage, deploy instructions
+- more security potentially in case harmful messages break the bot
+- implement proper checking for connections
+- implement proper logging
+- check for duplicate records
--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import os
+import psycopg2
+import psycopg2.extras
 
 import discord
 from dotenv import load_dotenv
@@ -6,7 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
 client = discord.Client()
+
+COMMAND_PREFIX = "treebark"
 
 @client.event
 async def on_ready():
@@ -16,8 +22,167 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    await message.channel.send('i work')
 
+    if message.content.startswith(COMMAND_PREFIX):
+        print(f'message {message.content} sent from guild {message.guild.id} from user {message.author.name}')
+        reply = run_command(message)
+        print(reply)
+        if reply is not None:
+            await message.channel.send(reply)
+
+def run_command(message):
+    args = message.content.split()
+    
+    if len(args) <= 1:
+        return "Hello! I help save useful Minecraft coordinates! I am a work in progress, so some things may not be so smooth, and I will also have more features in the future!\nAvailable commands:\n`treebark coords <world_name> <coords_tag (optional)>`\n`treebark record <world_name> <coords_tag> <x> <y> <z>`"
+
+    arg = args[1]
+    result = None
+
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        if arg == "coords":
+            if len(args) <= 2:
+                return None
+                # TODO return helpful message
+            elif len(args) == 3:
+                result = run_command_coords(cursor, args[2], message.guild.id)
+            elif len(args) == 4:
+                result = run_command_coords(cursor, args[2], message.guild.id, args[3])
+        elif arg == "record":
+            if len(args) < 7:
+                return None
+                #TODO return something helpful
+            elif len(args) == 7:
+                result = run_command_record(cursor, message.guild.id, args[2], args[3], args[4], args[5], args[6])
+            # TODO implement putting in description
+
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        result = "sorry! something went wrong while connecting to the server :("
+    finally:
+        if conn is not None:
+            conn.commit()
+            conn.close()
+    
+    print(result)
+    return result
+
+def run_command_coords(cursor, world, guild_id, search_tag=None):
+    if search_tag is None:
+        cursor.execute("""
+            SELECT 
+                * 
+            FROM 
+                coords
+            WHERE 
+                world = %(world)s
+            """, {
+                'world': world
+        })
+    else:
+        cursor.execute("""
+            SELECT 
+                * 
+            FROM 
+                coords
+            WHERE 
+                world = %(world)s
+                AND tag = %(search_tag)s
+            """, {
+                'world': world,
+                'search_tag': search_tag
+        })
+    
+    coord = cursor.fetchone()
+    reply = ""
+    while coord is not None:
+        print("coords command called")
+        print(coord)
+        
+        # TODO refactor to use join command
+        if coord["guild_id"] == guild_id:
+            reply += "World name: {}\nName: {}\nCoordinates: {} {} {}\nDescription: {}\n\n".format(coord["world"], coord["tag"], coord["x_coord"], coord["y_coord"], coord["z_coord"], coord["description"])
+
+        coord = cursor.fetchone()
+    
+    reply = reply.strip()
+    if reply == "":
+        reply = None
+    return reply
+
+def run_command_record(cursor, guild_id, world, tag, x, y, z, description=None):
+    try:
+        x = int(x)
+        y = int(y)
+        z = int(z)
+    except:
+        return "Looks like your command was typed incorrectly! Your command should look like `treebark record <world_name> <coords_tag> <x> <y> <z>`. Make sure there are no spaces in your world name or coordinates tag, and that coordinates are integers!"
+
+    if description is not None:
+        cursor.execute("""
+            INSERT INTO 
+                coords(
+                    guild_id, 
+                    world, 
+                    tag, 
+                    x_coord, 
+                    y_coord, 
+                    z_coord, 
+                    description
+                )
+            VALUES(
+                %(guild_id)s,
+                %(world)s,
+                %(tag)s,
+                %(x)s,
+                %(y)s,
+                %(z)s,
+                %(description)s
+            )
+        """, {
+            "guild_id": guild_id,
+            "world": world,
+            "tag": tag,
+            "x": x,
+            "y": y,
+            "z": z,
+            "description": description
+        })
+    else:
+        cursor.execute("""
+            INSERT INTO 
+                coords(
+                    guild_id, 
+                    world, 
+                    tag, 
+                    x_coord, 
+                    y_coord, 
+                    z_coord 
+                )
+            VALUES (
+                %(guild_id)s,
+                %(world)s,
+                %(tag)s,
+                %(x)s,
+                %(y)s,
+                %(z)s
+            )
+        """, {
+            "guild_id": guild_id,
+            "world": world,
+            "tag": tag,
+            "x": x,
+            "y": y,
+            "z": z,
+        })
+
+    reply = "Recorded coordinates:\nWorld name: {}\nName: {}\nCoordinates: {} {} {}\nDescription: {}\n\n".format(world, tag, x, y, z, description)
+    return reply
+    
 
 client.run(TOKEN)
